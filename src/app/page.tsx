2b1970b66bb11ed3133e4ace33c5dee2fdd45623
Mara T <mara.thdr@gmail.com>
2025-12-24 17:48:36 -0600

    Portfolio page fix and home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,22 @@ export default function Home() {
         <div className="absolute inset-0 bg-black/45" />
 
         <div className="relative z-10 max-w-5xl mx-auto px-6 py-24">
-          <h1 className="text-white text-3xl md:text-4xl lg:text-5xl leading-tight drop-shadow-lg font-[var(--font-playfair)]">
+          <h1 className="text-white text-3xl md:text-4xl lg:text-5xl leading-tight drop-shadow-lg font-(family-name:--font-cormorant-garamond) font-semibold text-center">
             Freelance photography service, providing portraits and event photography for individuals and groups within the local community
           </h1>
         </div>
+
       </section>
+
+      <div className="flex flex-1 justify-center">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSc9BB6GobF0SZtveCfMxI4sGimjrXnBAqqGMti1itTiDctyKw/viewform"
+        target="_blank">
+          <button className="px-8 py-4 mt-8 mb-12 bg-[rgb(255,251,244)] border-2 border-stone-800 text-stone-800 font-(family-name:--font-cormorant-garamond) font-semibold text-xl tracking-wide hover:bg-stone-800 hover:text-[rgb(255,251,244)] transition-all duration-300 shadow-md hover:shadow-xl">
+            Book a Session
+          </button>
+        </a>
+      </div>
+
     </main>
   );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+
+import PhotoAlbum from "react-photo-album";
+import "react-photo-album/styles.css";
+import * as React from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+
+
+const photos = [
+  {
+    src: "/img/birthday-cake.jpg",
+    width: 2455,
+    height: 1760,
+    alt: "birthday cake",
+  },
+  {
+    src: "/img/bubbles.jpg",
+    width: 1760,
+    height: 3120,
+    alt: "bubbles",
+  },
+  {
+    src: "/img/river.jpg",
+    width: 3120,
+    height: 1760,
+    alt: "river",
+  },
+  {
+    src: "/img/white-roses.jpg",
+    width: 3120,
+    height: 1760,
+    alt: "white roses",
+  },
+  {
+    src: "/img/sun-spot.jpg",
+    width: 1760,
+    height: 3120,
+    alt: "sun spot",
+  },
+  {
+    src: "/img/vines.jpg",
+    width: 1760,
+    height: 3120,
+    alt: "vines",
+  },
+];
+
 export default function Portfolio() {
+  const [index, setIndex] = React.useState(-1);
+
   return (
-    <div className="flex flex-1 justify-center items-center min-h-[70vh] bg-[rgb(255, 251, 244)]">
-      <h1 className="text-[rgba(118,83,66,0.71)] text-5xl">Portfolio</h1>
+    <div className="min-h-[70vh] bg-[rgb(255, 251, 244)] py-8">
+      <div className="container mx-auto px-6" style={{ maxWidth: '1400px' }}>
+        <PhotoAlbum
+          layout="rows"
+          photos={photos}
+          targetRowHeight={350}
+          spacing={15}
+          onClick={({ index }) => setIndex(index)}
+        />
+
+        <Lightbox
+          plugins={[Thumbnails]}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+          index={index}
+          slides={photos}
+        />
+      </div>
     </div>
   );
 }
